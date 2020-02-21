9bd156a6341f35f647cb682040b436430f0a9133
andrewstringfel <andrewstringfel@gmail.com>
2020-02-21 12:59:55 -0500

    Typing changes

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Andrew Stringfellow.", "a Student.", "a Designer.", "a Developer."],
       typeSpeed: 100,
       loop: true,
     });
